ef27fd3b4bfca37493b45b1949ecba6ddf0e16bc
Favo Yang <favoyang@gmail.com>
2023-11-10 01:03:50 +0800

    chore: remove build folder

--- packages/vuepress-plugin-openupm/build/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Page } from '@vuepress/types';
-declare const _default: () => {
-    name: string;
-    onInitialized(app: App): Promise<void>;
-    extendsPage: (page: Page) => Promise<void>;
-    onPrepared: (app: App) => Promise<void>;
-};
-export default _default;
