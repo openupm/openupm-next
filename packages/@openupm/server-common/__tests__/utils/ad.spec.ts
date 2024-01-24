4f6b3d6fbb319151d3e224617156266c4ac66dd0
Favo Yang <favoyang@gmail.com>
2024-01-25 00:57:46 +0800

    test(server-common): add test for utils/ad

---
+++ packages/@openupm/server-common/__tests__/utils/ad.spec.ts
@@ -0,0 +1,41 @@
+import config from 'config';
+import { convertAdAssetStoreToAdPlacementData } from '../../src/utils/ad.js';
+
+describe('convertAdAssetStoreToAdPlacementData', function () {
+  it('should convert AdAssetStore to AdPlacementData', async function () {
+    const result = convertAdAssetStoreToAdPlacementData({
+      id: 'id',
+      slug: 'slug',
+      title: 'title',
+      category: 'category',
+      image: 'image',
+      image2: 'image2',
+      icon: 'icon',
+      price: 'price',
+    });
+    expect(result).toEqual({
+      title: 'title',
+      image: 'image',
+      price: 'price',
+      url: `https://prf.hn/click/camref:${config.unityAffiliateId}/destination:https://assetstore.unity.com/packages/slug/slug`,
+    });
+  });
+  it('should use image2 if image is empty', async function () {
+    const result = convertAdAssetStoreToAdPlacementData({
+      id: 'id',
+      slug: 'slug',
+      title: 'title',
+      category: 'category',
+      image: '',
+      image2: 'image2',
+      icon: 'icon',
+      price: 'price',
+    });
+    expect(result).toEqual({
+      title: 'title',
+      image: 'image2',
+      price: 'price',
+      url: `https://prf.hn/click/camref:${config.unityAffiliateId}/destination:https://assetstore.unity.com/packages/slug/slug`,
+    });
+  });
+});
