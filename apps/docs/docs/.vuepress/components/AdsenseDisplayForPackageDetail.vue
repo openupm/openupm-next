e96cabc601877870cf6a9901d880ef070c26edad
Favo Yang <125390+favoyang@users.noreply.github.com>
2025-02-24 22:20:03 +0800

    chore(website): adjust Adsense display height for package detail page

--- apps/docs/docs/.vuepress/components/AdsenseDisplayForPackageDetail.vue
+++ apps/docs/docs/.vuepress/components/AdsenseDisplayForPackageDetail.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mb-2">
     <!-- openupm-pkgdetail-rightside -->
-    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1509006252899759" data-ad-slot="8471296007"
+    <ins class="adsbygoogle" style="display:block; height: 300px;" data-ad-client="ca-pub-1509006252899759" data-ad-slot="8471296007"
       data-ad-format="auto" data-full-width-responsive="true"></ins>
     <scriptx>
       (adsbygoogle = window.adsbygoogle || []).push({});
