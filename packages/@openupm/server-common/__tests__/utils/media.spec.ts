35ff07619ae34e59219bf29ceccda5c34f768f7b
Favo Yang <125390+favoyang@users.noreply.github.com>
2026-01-06 00:25:31 +0800

    test: update media utils spec

--- packages/@openupm/server-common/__tests__/utils/media.spec.ts
+++ packages/@openupm/server-common/__tests__/utils/media.spec.ts
@@ -1,8 +1,23 @@
-import { getImage } from '../../src/utils/media.js';
+import { jest } from '@jest/globals';
+
+const hgetallMock = jest.fn(async () => ({}));
+
+jest.unstable_mockModule('../../src/redis.js', () => ({
+  default: {
+    get client() {
+      return {
+        hgetall: hgetallMock,
+      };
+    },
+  },
+}));
+
+const { getImage } = await import('../../src/utils/media.js');
 
 describe('getImage()', function () {
   it('non-existed url', async function () {
     const result = await getImage('non-existed-url', 50, 50, 'cover');
     expect(result).toBeNull();
+    expect(hgetallMock).toHaveBeenCalledTimes(1);
   });
 });
