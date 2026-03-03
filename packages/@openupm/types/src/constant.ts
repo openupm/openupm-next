088868162f2036298114d75dfe00023b05f2df3b
Favo Yang <125390+favoyang@users.noreply.github.com>
2026-03-03 13:24:51 +0800

    feat(types): add submodule fetching release error

--- packages/@openupm/types/src/constant.ts
+++ packages/@openupm/types/src/constant.ts
@@ -53,6 +53,8 @@ export enum ReleaseErrorCode {
   RemoteRepositoryUnavailable = 807,
   // Fatal clone
   RemoteSubmoduleUnavailable = 808,
+  // Submodule fetch failed
+  SubmoduleFetchingError = 809,
   // Connection timeout
   ConnectionTimeout = 900,
   // Connection timeout
